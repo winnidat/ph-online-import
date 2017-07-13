3d6f5dc803c36e9764063f9bf2c9ff2f30641837
winidat <winkler_t@gmx.at>
2017-07-13 13:33:26 +0200

    dummy Token file

---
+++ ph-online-token.php
@@ -0,0 +1,16 @@
+<?php
+namespace PHOnlineToken;
+
+class Token {
+
+    function __construct(){
+    }
+    //add url + token for each PH
+    static function GetToken(){
+        return array(
+        	'phbgld' => 'dummy',
+        	'phooe' => 'dummy',
+        	);
+    }
+    
+}
